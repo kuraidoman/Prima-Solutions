d1295826a7948402715f7e094db2859969de1a56
ruruxhico <daniellejanm.15@gmail.com>
2026-08-01 02:10:12 +0800

    added button import

--- src/pages/Consult.tsx
+++ src/pages/Consult.tsx
@@ -1,6 +1,7 @@
 import { Phone, Mail, MapPin, FacebookIcon } from "lucide-react";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import { Button } from "@/components/ui/button";
 
 const contactDetails = [
   {
